466de65c253e5e57b52abc7cfb89fc8e96ed671e
orlando.tyrell <orlandotyrell@gmail.com>
2022-11-10 08:47:21 -0500

    Updates

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ require("dotenv").config();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var todosRouter = require('./routes/todos');
 
 var { mongoConnect } = require('./mongo.js');
 mongoConnect();
@@ -29,6 +30,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/todos', todosRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
